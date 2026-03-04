8e35d89aa05a2c4c5c6377ef8d71c06494394cb8
Susmitha-819 <susmithakondakayala@gmail.com>
2026-03-04 20:57:33 +0530

    Add files via upload

---
+++ p-34.py
@@ -0,0 +1,18 @@
+def num(arr):
+
+    even_num=arr[0]
+    odd_num=arr[0]
+
+    for num in arr:
+        if num % 2==0:
+    return even_num
+
+    for num in arr:
+        if num % 2==0:
+
+    return odd_num
+
+
+print("even_num"([1,2,3,4,5,6]))
+print("odd_even"([3,5,7,9,1,4]))
+        
