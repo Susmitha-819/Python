661fd364adb12b35be63ef931daa61d236515273
Susmitha-819 <susmithakondakayala@gmail.com>
2026-03-05 21:19:54 +0530

    Add files via upload

---
+++ p-43.py
@@ -0,0 +1,11 @@
+print(next(fruit_iterator))   #apple  
+print(next(fruit_iterator))   #banana
+print(next(fruit_iterator))   #cherry
+
+
+numbers =[1,2,3]
+num_iter=iter(numbers)
+
+print(next(num_iter))     #1
+print(next(num_iter))     #2
+print(next(num_iter))     #3
