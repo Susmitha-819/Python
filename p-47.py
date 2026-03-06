9c0bc35deb26455993e42a46e9e5502b67f746d4
Susmitha-819 <susmithakondakayala@gmail.com>
2026-03-06 21:52:14 +0530

    Add files via upload

---
+++ p-47.py
@@ -0,0 +1,21 @@
+class Dog:
+    def __init__(self,name,age,breed):
+        self.name=name
+        self.age=age
+        self.breed=breed
+
+dog1=Dog("bob",3,"shakesphere")
+dog2=Dog("alice",4,"lillysom")
+
+print(dog1)
+print(dog2)
+
+
+
+
+
+
+
+
+
+
