3a9638a3763cb6bdc9d060d4010acfbd5364cf41
Susmitha-819 <susmithakondakayala@gmail.com>
2026-03-19 17:15:51 +0530

    Add files via upload

---
+++ p-36.py
@@ -0,0 +1,29 @@
+def calculator(a,b):
+    add_num=a+b
+    sub_num=a-b
+    mul_num=a*b
+    div_num=a/b
+    return add_num ,sub_num, mul_num, div_num
+calculators=calculator(2,3)
+print(calculators)
+
+
+
+
+
+tuplea=(10,20,30)
+print(tuplea[1])
+
+
+
+
+str="susmitha"
+print(len(str))
+
+
+def student_detail(name,age,course,profession):
+    return name,age,course,profession
+student_details=student_detail(name="susmi",age=20,course="ai",profession="student")
+print(student_details)
+
+
